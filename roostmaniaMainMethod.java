b6b2e16dc0727670e70b2414a043ee7efdef6ff3
macOS Junkie <57506703+CoolGeek167@users.noreply.github.com>
2023-04-25 18:26:55 -0500

    Update roostmaniaMainMethod.java

--- roostmaniaMainMethod.java
+++ roostmaniaMainMethod.java
@@ -1,11 +1,16 @@
+import java.sql.Time;
 import java.util.concurrent.TimeUnit;
-public class roostmaniaMainMethod {
-    public static void main(String[] args) throws Exception {
-        printStory(); //Prints out story
+import java.util.Scanner;
+public class Main {
+
+    public static void roostmaniaMainMethod(String[] args) throws Exception {
+
+        //printStory(); //Prints out story.
+        difficultySelect(); //Selects difficulty for our chicken warrior.
     }
 
     public static void printStory() throws Exception {
-
+        Scanner in = new Scanner(System.in);
         String chickula = "   Chickula: ";
         String mrChicken = "Mr. Chicken: ";
 
@@ -13,6 +18,8 @@ public class roostmaniaMainMethod {
         printWithDelays("Copyright 1997...I mean 2023...wait a sec...it's open source!\n", TimeUnit.MILLISECONDS, 75);
         printWithDelays("This is a fan game and is not affiliated with Konami.\n", TimeUnit.MILLISECONDS, 75);
         System.out.println(); //Space
+        printWithDelays("Press RETURN or ENTER to begin your adventure!\n", TimeUnit.MILLISECONDS, 75);
+        String temp = in.nextLine();
         printWithDelays("In the year 1784, there once lived a vampire chicken hunter known as Mr. Chicken!\n", TimeUnit.MILLISECONDS, 60);
         printWithDelays("The terror known as Chickula torments members of the coop...\n", TimeUnit.MILLISECONDS, 60);
         System.out.println(); //Space
@@ -33,12 +40,33 @@ public class roostmaniaMainMethod {
 
     }
 
-    public static void printWithDelays(String data, TimeUnit unit, long delay)
+    public static void printWithDelays(String data, TimeUnit unit, long delay) //This method allows for delayed text printing through a time specificed with units.
             throws InterruptedException {
         for (char ch:data.toCharArray()) {
             System.out.print(ch);
             unit.sleep(delay);
         }
     }
-}
 
+    public static void difficultySelect() throws Exception
+    {
+        Scanner in = new Scanner(System.in);
+        boolean loopRun = true;
+        while(loopRun){
+        printWithDelays("", TimeUnit.MILLISECONDS, 100);
+        printWithDelays("\nMr. Chicken, pick your level...\n", TimeUnit.MILLISECONDS, 60);
+        printWithDelays("1: Boring mode...made for chickens\n2: Chicken warrior...recommended for most vampire chicken slayers\n3: Chicken roast... you'll be chicken dinner\n", TimeUnit.MILLISECONDS, 20);
+        double difficulty = in.nextInt();
+        if((difficulty > 3 || difficulty < 1))
+        {
+            System.out.println("That's not a difficulty level!");
+        }
+        else
+        {
+            loopRun = false;
+        }
+
+        }
+
+    }
+}
